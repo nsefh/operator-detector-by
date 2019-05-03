0929d4d33dcc926e8073406884d4d3c859ba9996
Artem Nester <artem.nester.m@gmail.com>
2019-05-03 17:45:56 +0300

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='foobar-package',
+    version='1.0.0',
+    packages=find_packages(),
+    author='John Smith',
+    author_email='johnsmith@example.com',
+    url='https://github.com/johnsmith/foobar-package',
+)
